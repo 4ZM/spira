392ae49ed1c1040f0410f3bab8fcd3736a6d445f
Anders Sundman <anders@4zm.org>
2013-10-12 16:12:20 +0200

    Using grunt & bower

---
+++ Gruntfile.js
@@ -0,0 +1,96 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            files: ['Gruntfile.js', 'presentation/app/js/*.js'],
+            options:{
+                globalstrict: true,
+                globals:{
+                    module: true, // grunt
+                    angular: true
+                },
+            },
+        },
+
+        // karma: {
+        //     unit: {
+        //         configFile: 'config/karma.conf.js',
+        //     },
+
+        //     continuous: {
+        //         configFile: 'config/karma.conf.js',
+        //         singleRun: true,
+        //         browsers: ['PhantomJS']
+        //     },
+        // },
+
+        // shell: {                                // Task
+        //     listFolders: {                      // Target
+        //         options: {                      // Options
+        //             stdout: true
+        //         },
+        //         command: 'ls'
+        //     }
+        // },
+
+        bower: {
+            install: {
+                options: {
+                    targetDir: 'presentation/app/lib',
+                    layout: 'byComponent',
+                    install: true,
+                    verbose: false,
+                    cleanTargetDir: false,
+                    cleanBowerDir: true
+                }
+            }
+        },
+
+        clean: {
+            release: ['<%= pkg.directories.release %>']
+        },
+        copy: {
+            release: {
+                files: [
+                    { expand: true,
+                      cwd: 'presentation/app/',
+                      src: ['**/*.html'],
+                      dest: '<%= pkg.directories.release %>'
+                    },
+                ]
+            }
+        },
+        useminPrepare: {
+            html: ['presentation/app/index.html'],
+            options: {
+                dest: '<%= pkg.directories.release %>'
+            }
+        },
+        usemin: {
+            html: ['<%= pkg.directories.release %>/index.html'],
+        }
+    });
+    
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    // grunt.loadNpmTasks('grunt-karma');
+    // grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-usemin');
+    
+    // Default task(s).
+    grunt.registerTask('default', ['jshint']);
+
+    grunt.registerTask('deploy', ['clean', 'copy', 'useminPrepare', 'concat', 'uglify', 'usemin']);
+    //grunt.registerTask('foo', ['concat', 'jshint']);
+};
--- README.md
+++ README.md
@@ -2,9 +2,18 @@
 
 En trädgårdsplanerare för amatörodlare
 
+## Beroenden
+Installera grunt, karma, bower och phantomjs globalt med npm.
+
+Kör sedan npm install för att installera övriga beroenden lokalt.
+
 ## Licens
 
 Copyright (C) 2013 Anders Sundman <anders@4zm.org>
 
 Spira är fri mjukvara och kan distribueras i enlighet med villkoren i
 GPL Gnu Public License version 3.
+
+
+
+
